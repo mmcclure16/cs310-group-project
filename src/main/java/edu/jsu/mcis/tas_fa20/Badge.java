d14e7554fb1cd6b0aca466a1311e726b2da72ded
Matthew McClure <mamcclure16@gmail.com>
2020-09-28 21:32:21 -0500

    Setup Badge Class

--- src/main/java/edu/jsu/mcis/tas_fa20/Badge.java
+++ src/main/java/edu/jsu/mcis/tas_fa20/Badge.java
@@ -3,4 +3,38 @@ package edu.jsu.mcis.tas_fa20;
 
 public class Badge {
 
+    private String id;
+    private String description;
+    
+    public Badge(String id, String description){
+        this.id = id;
+        this.description = description;
+    }
+    
+    public String getID(){
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder badge = new StringBuilder();
+        
+        badge.append("#").append(id).append(" ");
+        badge.append("(").append(description).append(")");
+        
+        return(badge.toString());
+    }
+    
 }
