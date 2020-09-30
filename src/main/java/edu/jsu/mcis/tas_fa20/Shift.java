9f6527ecae2a0e3681c9adc2469d966ca8c51d63
simon <shopkins2@stu.jsu.edu>
2020-09-30 13:10:36 -0400

    added shift variables

--- src/main/java/edu/jsu/mcis/tas_fa20/Shift.java
+++ src/main/java/edu/jsu/mcis/tas_fa20/Shift.java
@@ -1,6 +1,19 @@
 package edu.jsu.mcis.tas_fa20;
 
+import java.time.LocalTime;
+
 
 public class Shift {
-
+    private int id;
+    private String description;
+    private LocalTime start;
+    private LocalTime stop;
+    private int interval;
+    private int graceperiod;
+    private int dock;
+    private LocalTime lunchstart;
+    private LocalTime lunchstop;
+    private int lunchdeduct;
+    
+    
 }
