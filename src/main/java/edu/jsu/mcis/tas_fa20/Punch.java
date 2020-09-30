a38c59dea23cfcd54c3a9e4733d6a5b07afc5707
simon <shopkins2@stu.jsu.edu>
2020-09-30 12:31:40 -0400

    added punch variables

--- src/main/java/edu/jsu/mcis/tas_fa20/Punch.java
+++ src/main/java/edu/jsu/mcis/tas_fa20/Punch.java
@@ -2,5 +2,12 @@ package edu.jsu.mcis.tas_fa20;
 
 
 public class Punch {
-
+    private String id;
+    private String terminalid;
+    private String badgeid;
+    private Long originaltimestamp;
+    private String punchtypeid;
+    private String adjustmenttype;
+    
+    
 }
