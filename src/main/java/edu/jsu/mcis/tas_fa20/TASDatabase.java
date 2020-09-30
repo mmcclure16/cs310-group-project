5cce78e4f1a9667d1ace045ab1c87ae2a24bbdb8
Matthew McClure <mamcclure16@gmail.com>
2020-09-30 12:14:45 -0500

    Setup Database Connection

--- src/main/java/edu/jsu/mcis/tas_fa20/TASDatabase.java
+++ src/main/java/edu/jsu/mcis/tas_fa20/TASDatabase.java
@@ -1,10 +1,53 @@
 package edu.jsu.mcis.tas_fa20;
 
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class TASDatabase {
+    public TASDatabase(){
+        Connection conn = null;
+        PreparedStatement pstSelect = null, pstUpdate = null;
+        ResultSet resultset = null;
+        ResultSetMetaData metadata = null;
 
-    public static void main(String[] args) {
-       
+        String query;
+
+        boolean hasresults;
+        int resultCount, columnCount = 0;
+        
+        try {
+
+            /* Identify the Server */
+
+            String server = ("jdbc:mysql://localhost/tas");
+            String username = "tasuser";
+            String password = "CS488";
+            System.out.println("Connecting to " + server + "...");
+
+            /* Load the MySQL JDBC Driver */
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+
+            /* Open Connection */
+
+            conn = DriverManager.getConnection(server, username, password);
+
+            /* Test Connection */
+
+            if (conn.isValid(0)) {
+
+                /* Connection Open! */
+
+                System.out.println("Connected Successfully!");
+            }
+            
+        }catch (Exception e) {
+            System.err.println(e.toString());
+        } 
     }
-
 }
+        
+
+ 
