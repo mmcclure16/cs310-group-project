39abae9b5156a9f3796695be7730fedfae1265f0
Daniel Lester <dlester@stu.jsu.edu>
2020-11-09 16:40:00 -0600

    Made method to calc minutes logged during shift (Completed Feature 4)

--- src/tas_fa20/TASLogic.java
+++ src/tas_fa20/TASLogic.java
@@ -1,6 +1,32 @@
 package tas_fa20;
 
+import java.util.*;
+
+import static java.time.temporal.ChronoUnit.MINUTES;
+
 
 public class TASLogic {
-
+    
+    public static int calculateTotalMinutes(ArrayList<Punch> dailyPunchList, Shift shift) {
+        
+        int minsWorked = 0;
+        boolean hasNotHadScheduledLunch = true;
+        
+        for (int i = 0; i+1 < dailyPunchList.size(); i += 2) {
+            minsWorked += (
+                    dailyPunchList.get(i+1).getAdjustedTimeStamp()
+                    - dailyPunchList.get(i).getAdjustedTimeStamp()
+                    ) / 60000;
+            
+            if (hasNotHadScheduledLunch) hasNotHadScheduledLunch = dailyPunchList.get(i).isAdjustedPunchOutsideShiftLunch();
+        }
+        
+        if (hasNotHadScheduledLunch && (minsWorked > shift.getLunchDeduct())) {
+            minsWorked -= MINUTES.between(shift.getLunchStart(), shift.getLunchStop());
+        }
+        
+        return minsWorked;
+        
+    }
+            
 }
