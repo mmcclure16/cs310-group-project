e32a2e0e72a07c73fbfc36e693a960eaa1d9c613
Daniel Lester <dlester@stu.jsu.edu>
2020-11-20 13:37:15 -0600

    Implemented constructor and `String toString()` method

--- src/tas_fa20/Absenteeism.java
+++ src/tas_fa20/Absenteeism.java
@@ -1,9 +1,44 @@
 package tas_fa20;
 
+import java.time.*;
+import java.time.format.DateTimeFormatter; 
+
+import static java.time.DayOfWeek.SUNDAY;
+import static java.time.temporal.TemporalAdjusters.previous;
+
 public class Absenteeism {
     
-    public Absenteeism(String badgeID, Long timeStamp, double percentage) {
+    private String badgeID;
+    private double percentage;
+    
+    private LocalDate payPeriodDate;
+    
+    private static final DateTimeFormatter timeStampFormat  = DateTimeFormatter.ofPattern("MM-dd-yyyy");
+    
+    public Absenteeism(String badgeID, long timeStamp, double percentage) {
         
+        
+        /* Apply direct values */
+        
+        this.badgeID = badgeID;
+        this.percentage = percentage;
+        
+        
+        /* Set pay period start */
+        
+        Instant TS_Instant = Instant.ofEpochMilli(timeStamp);
+        payPeriodDate = TS_Instant.atZone(ZoneId.systemDefault()).toLocalDate();
+        //.with(previous(SUNDAY));
+        if (payPeriodDate.getDayOfWeek() != SUNDAY) {
+            payPeriodDate = payPeriodDate.with(previous(SUNDAY));
+        }
+        
+    }
+    
+    @Override
+    public String toString() {
+        return ("#" + badgeID + " (Pay Period Starting " + timeStampFormat.format(payPeriodDate) + ") : "
+                + percentage + "%");
     }
     
 }
